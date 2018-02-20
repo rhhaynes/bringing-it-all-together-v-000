f5bb2c04a399b9608462d990e7872d63d043c04e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-20 17:31:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,8 +60,8 @@ class Dog
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")[0][0]
-      self
     end
+    self
   end
   
   def update
