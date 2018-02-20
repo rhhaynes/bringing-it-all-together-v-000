c5c56574fe32309b1430812da1ecf2517883873b
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-20 16:38:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,16 @@ class Dog
   end
   
   def self.drop_table
+    sql = <<~SQL
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def initialize()
   end
   
 end
+
+sql = <<~SQL
+    SQL
+    DB[:conn].execute(sql)
